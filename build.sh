62b750b239d1ef875b5a3ab7b5a13d928911265c
johanvandegriff <johan.vandegriff@gmail.com>
2016-10-04 20:38:07 -0400

    Compiled 0.1.6 with java 7

--- build.sh
+++ build.sh
@@ -7,18 +7,43 @@
 
 if [[ -z "$1" ]] #if no version number was supplied
 then
-   echo "usage: $0 version.number"
-   exit 1
+  echo "usage: $0 version.number"
+  exit 1
 fi
 
-file="build/state-machine-framework-$1.jar" #the path to the jar file to be created/overwritten
-out="out/production/state-machine-framework/" #the build path where all the .class files are
+#the path to the jar file to be created/overwritten
+file="build/state-machine-framework-$1.jar"
 
-echo jar cf "$file" -C "$out" ftc/
-jar cf "$file" -C "$out" ftc/ #create the jar file
+#the build path where all the .class files are
+#out="out/production/state-machine-framework/"
+#out="build/"
+out="bin/"
+
+source_dir="src/"
+package="ftc/"
+
+#the source directory
+sources="build/state-machine-framework-$1-sources.jar"
+
+#the location of the project where the jar file should be copied to
+#dir="../ftc20151105/FtcRobotController/libs/"
+#dir="../ftc20151104/libs/"
+dir="$HOME/AndroidStudio/ftc7393/libs/"
+
+echo "<=== COMPILED ===>"
+echo jar cf "$file" -C "$out" "$package"
+jar cf "$file" -C "$out" "$package" #create the jar file
 #the -C option tells the utility to cd into $out before creating the jar file
 
-dir="../ftc20151104/FtcRobotController/libs/" #the location of the project where the jar file should be copied to
+echo "<=== SOURCES ===>"
+cd "$source_dir"
+zip -r "../$sources" "$package"
+cd ..
+
+echo "<=== GIT ADD ===>"
+git add "$file" "$sources"
+
+echo "<=== COPY ===>"
 
 if [[ ! -d "$dir" ]] #if the project is not there
 then
@@ -27,4 +52,8 @@ then
 fi
 
 echo cp "$file" "$dir"
-cp "$file" "$dir" #copy the file
+cp "$file" "$dir" #copy the jar file
+echo cp "$sources" "$dir"
+cp "$sources" "$dir" #copy the sources
+
+
