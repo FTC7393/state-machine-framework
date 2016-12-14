bc38b156918aef35fbfacace09c3fba66c8afa33
johanvandegriff <johan.vandegriff@gmail.com>
2016-12-14 18:11:49 -0500

    v0.5.6 added List and array joining methods to Utility

--- src/ftc/electronvolts/test/util/UtilityTest.java
+++ src/ftc/electronvolts/test/util/UtilityTest.java
@@ -2,6 +2,9 @@ package ftc.electronvolts.test.util;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 import ftc.electronvolts.util.Utility;
@@ -54,4 +57,75 @@ public class UtilityTest {
         assertEquals(1, Utility.servoLimit(1.5), 0);
     }
 
+    @Test
+    public void testJoinListOfTString() {
+        List<String> list = new ArrayList<>();
+        assertEquals("", Utility.join(list, "djhfaejlaekwrbfjladjv"));
+        list.add("item 1");
+        list.add("item 2");
+        list.add("item 3");
+        assertEquals("item 1, item 2, item 3", Utility.join(list, ", "));
+
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        assertEquals("1;2;3", Utility.join(list1, ";"));
+    }
+
+    @Test
+    public void testJoinBooleanArrayString() {
+        assertEquals("", Utility.join(new boolean[] {}, "32458y"));
+        assertEquals("true", Utility.join(new boolean[] { true }, "34857028"));
+        assertEquals("true-false-true-true", Utility.join(new boolean[] { true, false, true, true }, "-"));
+    }
+
+    @Test
+    public void testJoinByteArrayString() {
+        assertEquals("", Utility.join(new byte[] {}, "32458y"));
+        assertEquals("100", Utility.join(new byte[] { 100 }, "34857028"));
+        assertEquals("100 + 20 + -3 + -5 + -100", Utility.join(new byte[] { 100, 20, -3, -5, -100 }, " + "));
+    }
+
+    @Test
+    public void testJoinCharArrayString() {
+        assertEquals("", Utility.join(new char[] {}, "32458y"));
+        assertEquals("a", Utility.join(new char[] { 'a' }, "34857028"));
+        assertEquals("q//w//e//r//t//y", Utility.join(new char[] { 'q', 'w', 'e', 'r', 't', 'y' }, "//"));
+    }
+
+    @Test
+    public void testJoinShortArrayString() {
+        assertEquals("", Utility.join(new short[] {}, "32458y"));
+        assertEquals("800", Utility.join(new short[] { 800 }, "34857028"));
+        assertEquals("1 Mississippi 2 Mississippi 3 Mississippi 4 Mississippi 5", Utility.join(new short[] { 1, 2, 3, 4, 5 }, " Mississippi "));
+    }
+
+    @Test
+    public void testJoinIntArrayString() {
+        assertEquals("", Utility.join(new int[] {}, "32458y"));
+        assertEquals("200000", Utility.join(new int[] { 200000 }, "34857028"));
+        assertEquals("2345~567~123~678~43~67~789", Utility.join(new int[] { 2345, 567, 123, 678, 43, 67, 789 }, "~"));
+    }
+
+    @Test
+    public void testJoinLongArrayString() {
+        assertEquals("", Utility.join(new long[] {}, "32458y"));
+        assertEquals("492843750", Utility.join(new long[] { 492843750 }, "34857028"));
+        assertEquals("44#55#66", Utility.join(new long[] { 44, 55, 66 }, "#"));
+    }
+
+    @Test
+    public void testJoinFloatArrayString() {
+        assertEquals("", Utility.join(new float[] {}, "32458y"));
+        assertEquals("1.23456", Utility.join(new float[] { 1.23456f }, "34857028"));
+        assertEquals("3.3<>400.0<>123.123<>456.789", Utility.join(new float[] { 3.3f, 400f, 123.123f, 456.789f }, "<>"));
+    }
+
+    @Test
+    public void testJoinDoubleArrayString() {
+        assertEquals("", Utility.join(new double[] {}, "32458y"));
+        assertEquals("55.1", Utility.join(new double[] { 55.1 }, "34857028"));
+        assertEquals("123.0&33.5&10005.0&234.555", Utility.join(new double[] { 123, 33.5, 10005, 234.555 }, "&"));
+    }
 }
--- src/ftc/electronvolts/util/Utility.java
+++ src/ftc/electronvolts/util/Utility.java
@@ -1,8 +1,9 @@
 package ftc.electronvolts.util;
 
+import java.util.List;
+
 /**
- * This file was made by the electronVolts, FTC team 7393
- * Date Created: 2/17/16
+ * This file was made by the electronVolts, FTC team 7393 Date Created: 2/17/16
  */
 public class Utility {
     /**
@@ -58,6 +59,195 @@ public class Utility {
      */
     public static double servoLimit(double input) {
         return limit(input, 0, 1);
+    }
+
+    /**
+     * Join a list of objects with a separator
+     * 
+     * @param list the list of values
+     * @param separator the String to put between the values
+     * @return a String of the joined items
+     */
+    public static <T> String join(List<T> list, String separator) {
+        StringBuilder sb = new StringBuilder();
+        boolean first = true;
+        for (T item : list) {
+            if (first) {
+                first = false;
+            } else {
+                sb.append(separator);
+            }
+            sb.append(item);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Join a list of booleans with a separator
+     * 
+     * @param list the list of values
+     * @param separator the String to put between the values
+     * @return a String of the joined items
+     */
+    public static String join(boolean[] list, String separator) {
+        StringBuilder sb = new StringBuilder();
+        boolean first = true;
+        for (boolean item : list) {
+            if (first) {
+                first = false;
+            } else {
+                sb.append(separator);
+            }
+            sb.append(item);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Join a list of bytes with a separator
+     * 
+     * @param list the list of values
+     * @param separator the String to put between the values
+     * @return a String of the joined items
+     */
+    public static String join(byte[] list, String separator) {
+        StringBuilder sb = new StringBuilder();
+        boolean first = true;
+        for (byte item : list) {
+            if (first) {
+                first = false;
+            } else {
+                sb.append(separator);
+            }
+            sb.append(item);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Join a list of chars with a separator
+     * 
+     * @param list the list of values
+     * @param separator the String to put between the values
+     * @return a String of the joined items
+     */
+    public static String join(char[] list, String separator) {
+        StringBuilder sb = new StringBuilder();
+        boolean first = true;
+        for (char item : list) {
+            if (first) {
+                first = false;
+            } else {
+                sb.append(separator);
+            }
+            sb.append(item);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Join a list of shorts with a separator
+     * 
+     * @param list the list of values
+     * @param separator the String to put between the values
+     * @return a String of the joined items
+     */
+    public static String join(short[] list, String separator) {
+        StringBuilder sb = new StringBuilder();
+        boolean first = true;
+        for (short item : list) {
+            if (first) {
+                first = false;
+            } else {
+                sb.append(separator);
+            }
+            sb.append(item);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Join a list of ints with a separator
+     * 
+     * @param list the list of values
+     * @param separator the String to put between the values
+     * @return a String of the joined items
+     */
+    public static String join(int[] list, String separator) {
+        StringBuilder sb = new StringBuilder();
+        boolean first = true;
+        for (int item : list) {
+            if (first) {
+                first = false;
+            } else {
+                sb.append(separator);
+            }
+            sb.append(item);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Join a list of longs with a separator
+     * 
+     * @param list the list of values
+     * @param separator the String to put between the values
+     * @return a String of the joined items
+     */
+    public static String join(long[] list, String separator) {
+        StringBuilder sb = new StringBuilder();
+        boolean first = true;
+        for (long item : list) {
+            if (first) {
+                first = false;
+            } else {
+                sb.append(separator);
+            }
+            sb.append(item);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Join a list of floats with a separator
+     * 
+     * @param list the list of values
+     * @param separator the String to put between the values
+     * @return a String of the joined items
+     */
+    public static String join(float[] list, String separator) {
+        StringBuilder sb = new StringBuilder();
+        boolean first = true;
+        for (float item : list) {
+            if (first) {
+                first = false;
+            } else {
+                sb.append(separator);
+            }
+            sb.append(item);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Join a list of doubles with a separator
+     * 
+     * @param list the list of values
+     * @param separator the String to put between the values
+     * @return a String of the joined items
+     */
+    public static String join(double[] list, String separator) {
+        StringBuilder sb = new StringBuilder();
+        boolean first = true;
+        for (double item : list) {
+            if (first) {
+                first = false;
+            } else {
+                sb.append(separator);
+            }
+            sb.append(item);
+        }
+        return sb.toString();
     }
 
 }
