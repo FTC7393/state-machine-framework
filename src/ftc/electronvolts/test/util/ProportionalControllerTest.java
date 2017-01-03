7ca0892154b8d130d9d6a0263782f12bea7dc3ea
johanvandegriff <johan.vandegriff@gmail.com>
2017-01-03 13:51:26 -0500

    v0.7.6 improved ProportionalController

--- src/ftc/electronvolts/test/util/ProportionalControllerTest.java
+++ src/ftc/electronvolts/test/util/ProportionalControllerTest.java
@@ -36,17 +36,25 @@ public class ProportionalControllerTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void testProportionalController5() {
-        new ProportionalController(0.1, 0.3, 0.2, 0.7);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testProportionalController6() {
         new ProportionalController(0.1, 0.3, 0.5, 0.4);
     }
 
     @Test
+    public void testProportionalController6() {
+        new ProportionalController(0.1, 0.3, 0.4, 0.5, 0.7);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testProportionalController7() {
+        new ProportionalController(0.1, 0.3, -0.4, 0.5, 0.7);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testProportionalController8() {
+        new ProportionalController(0.1, 0.4, 0.3, 0.5, 0.7);
+    }
+
+    @Test
     public void testComputeCorrection() {
-        ControlLoop c = new ProportionalController(0.1, 0.31, 0.5, 0.7);
+        ControlLoop c = new ProportionalController(0.1, 3.1, 0.5, 0.7);
         assertEquals(0, c.computeCorrection(0, 1), 0);
         assertEquals(0, c.computeCorrection(0, 2), 0);
         assertEquals(0, c.computeCorrection(0, 3), 0);
@@ -63,6 +71,30 @@ public class ProportionalControllerTest {
         assertEquals(0.5, c.computeCorrection(0, -4), 0);
         assertEquals(0.5, c.computeCorrection(0, -5), 0);
         assertEquals(0.6, c.computeCorrection(0, -6), 1e-10);
+        assertEquals(0.7, c.computeCorrection(0, -7), 0);
+        assertEquals(0.7, c.computeCorrection(0, -8), 0);
+        assertEquals(0.7, c.computeCorrection(0, -9), 0);
+    }
+
+    @Test
+    public void testComputeCorrection1() {
+        ControlLoop c = new ProportionalController(0.1, 3.1, 6, 0.5, 0.7);
+        assertEquals(0, c.computeCorrection(0, 1), 0);
+        assertEquals(0, c.computeCorrection(0, 2), 0);
+        assertEquals(0, c.computeCorrection(0, 3), 0);
+        assertEquals(-0.5, c.computeCorrection(0, 4), 0);
+        assertEquals(-0.5, c.computeCorrection(0, 5), 0);
+        assertEquals(-0.7, c.computeCorrection(0, 6), 1e-10);
+        assertEquals(-0.7, c.computeCorrection(0, 7), 0);
+        assertEquals(-0.7, c.computeCorrection(0, 8), 0);
+        assertEquals(-0.7, c.computeCorrection(0, 9), 0);
+
+        assertEquals(0, c.computeCorrection(0, -1), 0);
+        assertEquals(0, c.computeCorrection(0, -2), 0);
+        assertEquals(0, c.computeCorrection(0, -3), 0);
+        assertEquals(0.5, c.computeCorrection(0, -4), 0);
+        assertEquals(0.5, c.computeCorrection(0, -5), 0);
+        assertEquals(0.7, c.computeCorrection(0, -6), 1e-10);
         assertEquals(0.7, c.computeCorrection(0, -7), 0);
         assertEquals(0.7, c.computeCorrection(0, -8), 0);
         assertEquals(0.7, c.computeCorrection(0, -9), 0);
--- src/ftc/electronvolts/util/ProportionalController.java
+++ src/ftc/electronvolts/util/ProportionalController.java
@@ -6,17 +6,18 @@ package ftc.electronvolts.util;
  * A proportional controller to use for controlling motors and other outputs
  */
 public class ProportionalController implements ControlLoop {
-    private final double gain, maxOutput, deadzone, minOutput;
+    private final double gain, innerDeadzone, outerDeadzone, minOutput,
+            maxOutput;
 
-    public ProportionalController(double gain, double deadzone, double minOutput, double maxOutput) {
+    private static void checkParameters(double gain, double innerDeadzone, double minOutput, double maxOutput) {
         if (gain < 0) {
             throw new IllegalArgumentException("Illegal gain constant \"" + gain + "\". constants cannot be negative.");
         }
         if (maxOutput < 0) {
             throw new IllegalArgumentException("Illegal maxOutput constant \"" + maxOutput + "\". constants cannot be negative.");
         }
-        if (deadzone < 0) {
-            throw new IllegalArgumentException("Illegal deadzone constant \"" + deadzone + "\". constants cannot be negative.");
+        if (innerDeadzone < 0) {
+            throw new IllegalArgumentException("Illegal deadzone constant \"" + innerDeadzone + "\". constants cannot be negative.");
         }
         if (minOutput < 0) {
             throw new IllegalArgumentException("Illegal minOutput constant \"" + minOutput + "\". constants cannot be negative.");
@@ -24,29 +25,53 @@ public class ProportionalController implements ControlLoop {
         if (minOutput > maxOutput) {
             throw new IllegalArgumentException("minOutput (" + minOutput + ") cannot be grater than maxOutput (" + maxOutput + ")");
         }
-        if (deadzone > minOutput) {
-            throw new IllegalArgumentException("deadzone (" + deadzone + ") cannot be grater than minOutput (" + minOutput + ")");
+    }
+
+    public ProportionalController(double gain, double innerDeadzone, double minOutput, double maxOutput) {
+        checkParameters(gain, innerDeadzone, minOutput, maxOutput);
+
+        this.gain = gain;
+        this.maxOutput = maxOutput;
+        this.innerDeadzone = innerDeadzone;
+        this.outerDeadzone = -1;
+        this.minOutput = minOutput;
+    }
+
+    public ProportionalController(double gain, double innerDeadzone, double outerDeadzone, double minOutput, double maxOutput) {
+        checkParameters(gain, innerDeadzone, minOutput, maxOutput);
+
+        if (outerDeadzone < 0) {
+            throw new IllegalArgumentException("Illegal outerDeadzone constant \"" + outerDeadzone + "\". constants cannot be negative.");
+        }
+
+        if (innerDeadzone > outerDeadzone) {
+            throw new IllegalArgumentException("innerDeadzone (" + innerDeadzone + ") cannot be grater than outerDeadzone (" + outerDeadzone + ")");
         }
 
         this.gain = gain;
         this.maxOutput = maxOutput;
-        this.deadzone = deadzone;
+        this.innerDeadzone = innerDeadzone;
+        this.outerDeadzone = outerDeadzone;
         this.minOutput = minOutput;
     }
 
     @Override
     public double computeCorrection(double setPoint, double input) {
-        double correction = gain * (setPoint - input);
+        double error = setPoint - input;
+        double correction = gain * error;
 
-        if (Math.abs(correction) <= deadzone) {
+        if (Math.abs(error) <= innerDeadzone) {
             //return 0 if the error is in the deadzone
             return 0;
+        } else if (outerDeadzone > 0 && Math.abs(error) >= outerDeadzone) {
+            //return maxOutput if the error is in the outer deadzone
+            return Math.signum(error) * maxOutput;
         } else if (Math.abs(correction) < minOutput) {
             //return the minimum if it is below
-            return Math.signum(correction) * minOutput;
+            return Math.signum(error) * minOutput;
         } else if (Math.abs(correction) > maxOutput) {
             //cap the correction at +/- maxOutput
-            return Math.signum(correction) * maxOutput;
+            return Math.signum(error) * maxOutput;
         } else {
             return correction;
         }
